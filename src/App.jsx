5e910d000f719bcf7d89dc6dafaeb8cf74a57a24
Hadi Zare <hadizareoriginal@gmail.com>
2025-02-19 13:53:27 +0330

    remove comment

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,3 @@ const App = () =>  {
 }
 
 export default App
-
-// useEffect(() => {
-//     fetch('https://dummyjson.com/users')
-//         .then(res => res.text())
-//         .then(res => JSON.parse(res))
-//         .then(res => console.log(res))
-// }, []);
