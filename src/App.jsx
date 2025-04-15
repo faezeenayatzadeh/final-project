a11fb0206aba41b34b48bf9af136e5fa7152dd7a
Hadi Zare <hadizareoriginal@gmail.com>
2025-04-15 17:56:55 +0330

    feat: dynamic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,60 @@
+import PanelLayout from '@/layout/PanelLayout/PanelLayout';
 import ProductDetailPage from '@/pages/client/ProductDetailPage/ProductDetailPage';
-import AuthContainer from '@/pages/panel/Auth/Auth';
 import MyProfile from '@/pages/panel/Profile/MyProfile/MyProfile';
 import ProfileUsers from '@/pages/panel/Profile/Users/ProfileUsers';
+import links from '@/routes/links';
 import { Route, Routes } from 'react-router-dom';
 import './index.css';
 
+const client_pages = [
+  {
+    path: links.client.home,
+    element: <div>Home</div>,
+  },
+  {
+    path: links.client.product,
+    element: <ProductDetailPage />,
+  },
+]
+
+const panel_pages = [
+  {
+    isIndex: true,
+    element: <div>Home</div>,
+  },
+  {
+    path: links.panel.profile,
+    element: <MyProfile />,
+  },
+  {
+    path: links.panel.users,
+    element: <ProfileUsers />,
+  }
+]
+ 
 const App = () =>  {
   const isAuth = localStorage.getItem('token') ? true : false;
 
     return (
         <Routes>
-            <Route path="/" element={<div>Home</div>} />
-            <Route path="/product/:id" element={<ProductDetailPage />} />
-            <Route path="/auth" element={<AuthContainer />} />
+            {client_pages.map((page) => (
+                <Route key={page.path} path={page.path} element={page.element} />
+            ))}
             {isAuth && (
-              <Route path="/panel">
-                <Route index element={<div>panel</div>} />
-                <Route path="profile" element={<MyProfile />} />
-                <Route path="users" element={<ProfileUsers />} />
-              </Route>
+                <Route path="/panel" element={<PanelLayout />}>
+                  {
+                    panel_pages.map((page) => {
+                      if (page.isIndex) {
+                        return (
+                          <Route key='index-panel' index element={page.element} />
+                        )
+                      }
+                      return (
+                        <Route key={page.path} path={page.path} element={page.element} />
+                      )
+                    })
+                  }
+                </Route>
             )}
             <Route path="*" element={<div>404 - Page Not Found</div>} />
         </Routes>
